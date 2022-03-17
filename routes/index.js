5ddf86a1dc1713110e46acf27f841350d284dbb5
Milutin Jovanović <mjp@voreni.com>
2022-03-17 15:43:48 -0400

    json response

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.send('main page');
+  res.json({
+    "data": "this is where data will go"
+  })
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json([
+  {
+    number: 1,
+    name: 'Jane',
+    gender: 'female'
+  },
+  {
+    number: 2,
+    name: 'John',
+    gender: 'male'
+  }
+  ])
 });
 
 module.exports = router;
